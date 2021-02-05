a9d715e3e60a5fdc5fec1e497730ef9df19ee0e9
Avinash Murmu <avimurmu18@gmail.com>
2021-02-05 15:26:22 +0530

    responsive-navbar added

---
+++ responsive-navbar/js/main.js
@@ -0,0 +1,47 @@
+
+// Activate Menu through Hamberger Button
+function activateMenu(){
+	document.querySelector("#main-navbar .menu").classList.toggle("d-block");
+}
+
+
+// check if mobile viewport is matching and
+// do the toggling of menus and submenus.
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+  	//Here is Where our all the navigation code goes.
+  	var anchorElements = document.querySelectorAll("#main-navbar li > a");
+
+  	for (var i = 0; i < anchorElements.length; i++) {
+  		// Adding click event listener to all the 'a' tags
+  		anchorElements[i].addEventListener("click", function(){
+
+  			// check if the ul has children
+  			if (this.parentNode.children.length == 2) {
+  				// select the next element after anchor tag i.e. ul.sub-menu and toggle d-block if it is hidden
+          // but if it already has d-block then check if it contains any children containing d-block
+          // if it contains d-block remove it. So, what are we going to do here is check 
+          // if current clicked anchor's parent is containing any element inside it with class d-block.
+          // Remove It.
+          if(!(this.nextElementSibling.classList.contains("d-block"))){
+    				this.nextElementSibling.classList.add("d-block");
+          }
+          else{
+            var insideElementsWithDBlock = this.nextElementSibling.querySelectorAll(".d-block");
+            // if you want to see the elements
+            // console.log(insideElementsWithDBlock);
+            insideElementsWithDBlock.forEach(item => {
+              item.classList.remove("d-block");
+            });
+            this.nextElementSibling.classList.remove("d-block");
+          }
+  			}
+  		});
+  	}
+
+  }
+}
+
+var x = window.matchMedia("(max-width: 1199.98px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
